2b37e734cd46e90966e58a447aae1e28e2c67f97
Paul Niemann <palo.niemann@t-online.de>
2024-01-20 13:06:43 +0100

    structure

--- src/game_object.py
+++ src/game_object.py
@@ -1,8 +1,17 @@
+import pygame as pg
+
 class GameObject():
 
-    def __init__(x, y, w, h):
+    def __init__(self, x: float, y: float, w: int, h:int, img: str):
         self.x = x
         self.y = y
 
         self.width = w
         self.height = h
+        self.img = pg.image.load(img)
+
+    def update(self):
+        return
+    
+    def draw(self):
+        return
