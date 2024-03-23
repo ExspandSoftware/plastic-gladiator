b6c22d8ad113e077c81815a21355c07ce0a51087
Paul Niemann <palo.niemann@t-online.de>
2024-03-23 18:10:32 +0100

    innventoy

--- src/screens/inventory.py
+++ src/screens/inventory.py
@@ -1,5 +1,7 @@
 import pygame
 
+from functions.basic_rect import basic_rect
+
 from config import *
 
 class Inventory(pygame.sprite.Sprite):
@@ -15,19 +17,41 @@ class Inventory(pygame.sprite.Sprite):
         self.height = Iheight
         
         # other vars
-        self.number_of_items = 5
+        self.items = [
+            [pygame.image.load(os.path.join(WORKING_DIR, "assets", "images", "pre_edeka", "candy_crush", "Plastik-Flasche-mit-Pfand.png")), 0],
+            [pygame.image.load(os.path.join(WORKING_DIR, "assets", "images", "edeka", "space", "bag.png")), 1],
+            [pygame.image.load(os.path.join(WORKING_DIR, "assets", "images", "edeka", "space", "bag.png")), 3],
+            [pygame.image.load(os.path.join(WORKING_DIR, "assets", "images", "edeka", "space", "bag.png")), 1],
+        ]
+        for item in self.items:
+            item[0] = pygame.transform.scale(item[0], (Iheight*0.12, Iheight*0.12))
+        self.number_of_items = len(self.items)
         self.interface = pygame.Surface((Iheight*0.2*self.number_of_items, Iheight*0.2))
-        inventory_frame = pygame.image.load(os.path.join(WORKING_DIR, "assets", "images", "buttons", "inventory_frame.png"))
-        inventory_frame = pygame.transform.scale(inventory_frame, (Iheight*0.2, Iheight*0.2))
+        #self.inventory_frame = pygame.image.load(os.path.join(WORKING_DIR, "assets", "images", "buttons", "inventory_frame.png"))
+        #self.inventory_frame = pygame.transform.scale(self.inventory_frame, (Iheight*0.2, Iheight*0.2))
+        self.inventory_frame = basic_rect(Iheight*0.2, Iheight*0.2)
         #draw the inventory frames
         for i in range(self.number_of_items):
-            self.interface.blit(inventory_frame, (Iheight*0.2*i, 0))
+            self.interface.blit(self.inventory_frame, (Iheight*0.2*i, 0))
+
+        self.font = pygame.font.Font(os.path.join(WORKING_DIR, 'assets', 'fonts', 'game-font.ttf'), 20)
 
         self.image = pygame.Surface((Iwidth, Iheight), pygame.SRCALPHA)
         self.image.fill((0, 0, 0, 150))
         self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
     def update(self, Iwidth:int, Iheight:int, Cwidth:int, Cheight:int, *args, **kwargs):
+        
+        #drawing the items into the inventory
+        #draw the inventory frames
+        for i in range(self.number_of_items):
+            self.interface.blit(self.inventory_frame, (Iheight*0.2*i, 0))
+            self.interface.blit(self.items[i][0], (Iheight*0.2*i + Iheight*0.1 - self.items[i][0].get_width()//2, Iheight*0.1 - self.items[i][0].get_height()//2))
+
+            text = self.font.render(str(self.items[i][1])+"x", True, (255, 255, 255))
+            self.interface.blit(text, (Iheight*0.2*i + Iheight*0.15 - text.get_width(), Iheight*0.16 - text.get_height()))
+
+
         #setting up the interface
         self.image.blit(self.interface, self.interface.get_rect(topleft=(Iwidth//2 - self.interface.get_width()//2, Iheight//2 - self.interface.get_height()//2)))
 
